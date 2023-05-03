69d366fdea53377f280c2690560b99a5015c41b2
farhanpermana <46885640+farhanpermana@users.noreply.github.com>
2023-05-03 19:47:06 +0700

    Update HomeViewModel.swift

--- FinalProject/Modules/Home Page/ViewModel/HomeViewModel.swift
+++ FinalProject/Modules/Home Page/ViewModel/HomeViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+protocol ApiServiceStoreProtocol {
+    var urlString: String? { get set }
+    var bindListData: ((StoreModel?) -> ())? {get set}
+    func fetchItem()
+}
+
+class HomeViewModel: ApiServiceStoreProtocol {
+    
+    private var apiService: ApiService?
+    var data: StoreModel?
+    var urlString: String?
+    var bindListData: ((StoreModel?) -> Void)?
+    init(urlString: String?, apiService: ApiService?){
+        self.urlString = urlString
+        self.apiService = apiService
+        if let url = URL(string: urlString ?? "") {
+            self.apiService?.get(url: url)
+        }
+        fetchItem()
+    }
+    
+    func fetchItem() {
+        self.apiService?.callAPI(model: StoreModel?.self, completion: { response in
+            switch response {
+            case .success(let success):
+                self.bindListData?(success)
+                print("sukses")
+            case .failure(_):
+                print("gagal")
+                self.bindListData?(nil)
+            }
+        })
+    }
+    
+//    func numberOfRowsInSection() -> Int {
+//
+//    }
+}
